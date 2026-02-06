5fb4bf8ef4da3420f8cc97903539fc8714ecf7ee
York Chan <yorkchan@Yorks-MacBook-Pro.local>
2026-02-06 13:04:58 +1100

    Fix portal invoice sorting v15 context dict

--- portal_overrides/hooks.py
+++ portal_overrides/hooks.py
@@ -178,9 +178,10 @@ app_license = "mit"
 # 	"frappe.desk.doctype.event.event.get_events": "portal_overrides.event.get_events"
 # }
 override_whitelisted_methods = {
-    "erpnext.accounts.doctype.sales_invoice.sales_invoice.get_list_context":
-        "portal_overrides.sales_invoice.get_list_context"
+  "erpnext.accounts.doctype.sales_invoice.sales_invoice.get_list_context":
+    "portal_overrides.portal_overrides.sales_invoice.get_list_context"
 }
+
 #
 # each overriding function accepts a `data` argument;
 # generated from the base implementation of the doctype dashboard,
--- portal_overrides/sales_invoice.py
+++ portal_overrides/sales_invoice.py
@@ -1,6 +1,10 @@
-from erpnext.accounts.doctype.sales_invoice.sales_invoice import get_list_context as original
+from erpnext.accounts.doctype.sales_invoice import sales_invoice as erpnext_sales_invoice
 
 def get_list_context(context=None):
-    context = original(context)
-    context.order_by = "posting_date desc, name desc"
-    return context
+    # call ERPNext's original portal logic
+    ctx = erpnext_sales_invoice.get_list_context(context)
+
+    # override sorting for portal invoice list
+    ctx["order_by"] = "posting_date desc, name desc"
+
+    return ctx
